2719c75b0cfe47d90cb8d2aaeb2831a4f93efc03
Oliver Stuart <50858585+OliverPStuart@users.noreply.github.com>
2024-08-15 17:20:46 +0100

    Add files via upload

---
+++ config.R
@@ -0,0 +1,9 @@
+# R config file for capture paper scripts for thesis
+
+HOME_DIR="/Volumes/Alter/Daus_Phenotype_Evolution"
+DATA_DIR=paste0(HOME_DIR,"/Data")
+SCRIPT_DIR=paste0(HOME_DIR,"/Scripts")
+FIGURE_DIR=paste0(HOME_DIR,"/Figures")
+WORKING_DIR=paste0(HOME_DIR,"/Analysis")
+
+# The relevant script will then define a working directory, usually the containing directory
